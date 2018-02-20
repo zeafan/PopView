c697a3cd5128897c0b6030a053b37d86fc0df405
mohamed <zeafanm@gmail.com>
2018-02-20 02:03:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mohamed/FirebaseService/MyfireBaseService.java
@@ -0,0 +1,8 @@
+package com.example.mohamed.FirebaseService;
+
+/**
+ * Created by mohamed on 2/20/2018.
+ */
+
+public class MyfireBaseService {
+}
---
+++ app/src/main/java/com/example/mohamed/popview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mohamed.popview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+//SmallBangView smallBangView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  smallBangView
+      //  final PopField popField = PopField.attach2Window(this);
+       // PulsatorLayout pulsator = (PulsatorLayout) findViewById(R.id.pulsator);
+       // pulsator.start();
+//        LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext()
+//                .getSystemService(LAYOUT_INFLATER_SERVICE);
+//        final View addView = layoutInflater.inflate(R.layout.view2, null);         //Inflate new view from xml
+//        TextView newTextView = (TextView) addView.findViewById(R.id.txtView2);    //Reference the newview
+//       final TextView oldTxt=findViewById(R.id.txtV1);
+//        oldTxt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                popField.popView(oldTxt,addView);
+//            }
+//        });
+//        newTextView.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "new View", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+    }
+}
